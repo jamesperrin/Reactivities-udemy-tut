85b973d7d2dc7d417f7d7a3d18d84d81ac6356bb
James Perrin <countrydawgg@gmail.com>
2021-06-15 12:12:57 -0700

    Update files to begin development

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -44,7 +44,11 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            if (!env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
+            
 
             app.UseRouting();
 
