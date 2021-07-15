e066412414452f14743053e8f62508490cfd734c
James Perrin <countrydawgg@gmail.com>
2021-07-15 11:54:10 -0700

    Add LoadingComponent

--- src/client-app/src/app/layout/App.tsx
+++ src/client-app/src/app/layout/App.tsx
@@ -4,21 +4,24 @@ import { Activity } from '../models/activities';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { v4 as uuid } from 'uuid';
-import agent from '../api/agents';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
 	const [activities, setActivities] = useState<Activity[]>([]);
 	const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 	const [editMode, setEditMode] = useState(false);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		agent.Activities.list().then((response) => {
 			let activities: Activity[] = [];
-			response.forEach(activity => {
+			response.forEach((activity) => {
 				activity.date = activity.date.split('T')[0];
 				activities.push(activity);
 			});
 			setActivities(response);
+			setLoading(false);
 		});
 	}, []);
 
@@ -50,6 +53,10 @@ function App() {
 
 	function hanldeDeleteActivity(id: string) {
 		setActivities([...activities.filter((x) => x.id !== id)]);
+	}
+
+	if (loading) {
+		return <LoadingComponent content='Loading app' />;
 	}
 
 	return (
