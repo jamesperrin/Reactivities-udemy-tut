bacdab5e9bbe3cbf8c71ae2f08e28da6c3c72d7b
James Perrin <countrydawgg@gmail.com>
2021-09-14 18:09:55 -0700

    End of Section 10

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -19,6 +19,7 @@ using AutoMapper;
 using Application.Core;
 using API.Extensions;
 using FluentValidation.AspNetCore;
+using API.Middleware;
 
 namespace API
 {
@@ -46,7 +47,8 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers().AddFluentValidation(config =>{
+            services.AddControllers().AddFluentValidation(config =>
+            {
                 config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
             services.AddApplicationServices(_config);
@@ -55,9 +57,11 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
--- src/client-app/src/app/layout/App.tsx
+++ src/client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import NavBar from './NavBar';
@@ -7,26 +7,32 @@ import HomePage from '../../features/home/HomePage';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import ActivityForm from '../../features/activities/form/ActivityForm';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
 	const location = useLocation();
 
 	return (
 		<Fragment>
-			<Route exact path='/' component={HomePage} />
+			<ToastContainer position='bottom-right' hideProgressBar />
 			<Route
 				path={'/(.+)'}
 				render={() => (
 					<Fragment>
 						<NavBar />
 						<Container style={{ marginTop: '7em' }}>
-							<Route exact path='/activities' component={ActivityDashboard} />
-							<Route exact path='/activities/:id' component={ActivityDetails} />
-							<Route
-								key={location.key}
-								path={['/createActivity', '/manage/:id']}
-								component={ActivityForm}
-							/>
+							<Switch>
+								<Route exact path='/activities/:id' component={ActivityDetails} />
+								<Route
+									key={location.key}
+									path={['/createActivity', '/manage/:id']}
+									component={ActivityForm}
+								/>
+								<Route path='/server-error' component={ServerError} />
+								<Route component={NotFound} />
+							</Switch>
 						</Container>
 					</Fragment>
 				)}
