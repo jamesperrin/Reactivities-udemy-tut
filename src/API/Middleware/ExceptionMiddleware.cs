65a66b9862f246ac0f56a19d6603c6c45cdb4f5a
James Perrin <countrydawgg@gmail.com>
2021-10-06 10:17:55 -0700

    Fix broken ExceptionMiddleware

--- src/API/Middleware/ExceptionMiddleware.cs
+++ src/API/Middleware/ExceptionMiddleware.cs
@@ -1,55 +1,62 @@
+﻿using Application.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Application.Core;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace API.Middleware
 {
     public class ExceptionMiddleware
     {
-
-
         private readonly RequestDelegate _next;
-        private readonly ILogger<ExceptionMiddleware> _logger;
         private readonly IHostEnvironment _env;
+        private readonly ILogger<ExceptionMiddleware> _logger;
 
-        public ExceptionMiddleware(RequestDelegate next,
-        ILogger<ExceptionMiddleware> logger,
-        IHostEnvironment env)
+        public ExceptionMiddleware(RequestDelegate next, IHostEnvironment env, ILogger<ExceptionMiddleware> logger)
         {
+            _next = next;
             _env = env;
             _logger = logger;
-            _next = next;
-
         }
 
-        public async Task InvokeAysnc(HttpContext context)
+        public async Task Invoke(HttpContext context)
         {
             try
             {
-                await _next(context);
-
+                await _next.Invoke(context);
             }
             catch (Exception ex)
             {
+                HttpResponse response = context.Response;
                 _logger.LogError(ex, ex.Message);
-                context.Response.ContentType = "application/json";
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                response.ContentType = "application/json";
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-                var response = _env.IsDevelopment()
-                ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                : new AppException(context.Response.StatusCode, "Server Error");
+                AppException errorResponse = _env.IsDevelopment()
+                    ? new AppException(response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new AppException(response.StatusCode, "Server Error");
 
-                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                JsonSerializerOptions options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                string errorJson = JsonSerializer.Serialize(errorResponse, options);
 
-                var json = JsonSerializer.Serialize(response, options);
-
-                await context.Response.WriteAsync(json);
+                await response.WriteAsync(errorJson);
+                throw;
             }
         }
     }
+
+#if NOT_IMPLEMENTED
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    } 
+#endif
 }
